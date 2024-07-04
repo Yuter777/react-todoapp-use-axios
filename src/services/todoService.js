9807eb340e3610c732f0cb08fcad77fac007670d
Muhammad Tursunxojayev <147787386+Yuter777@users.noreply.github.com>
2024-07-04 19:36:40 +0500

    Update todoService.js

--- src/services/todoService.js
+++ src/services/todoService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/posts";
+const API_URL = "http://localhost:3000/posts";
 
 export const getTodos = async () => {
   const res = await axios.get(API_URL);
